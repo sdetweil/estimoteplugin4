5f36b934a402e5091bcd1da55fa07045fbaeff1c
sam detweiler <sdetweil@gmail.com>
2024-05-16 13:01:16 -0500

    add one code line to be uncommented for with 3rd party lib.

--- android/src/main/java/com/sd/plugins/estimoteplugin/EstimotePlugin.kt
+++ android/src/main/java/com/sd/plugins/estimoteplugin/EstimotePlugin.kt
@@ -18,7 +18,11 @@ class EstimotePlugin {
 
     //private var job: Job ? = null;
     private final var ourContext: AppCompatActivity?= null;
-    private val managerHandles = hashMapOf<String,String /*EstimoteUWBManager*/>();
+// uncomment next line when using 3rd party lib
+    //private val managerHandles = hashMapOf<String,EstimoteUWBManager>();
+// this same approach is used on the ios version of the plugin which has worked
+// comment out next line when using 3rd party libs
+    private val managerHandles = hashMapOf<String,String>();
 
     private fun getMS(): String{
         return Calendar.getInstance().timeInMillis.toString();
